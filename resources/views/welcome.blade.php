1f8ef11338a33dd7f96ca6a7512ead8bd3fded8a
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 16:22:45 +0200

    delete blog folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel deploy prod!!
+                    Laravel deploy 54!!
                 </div>
 
                 <div class="links">
