6e76f707ec045760589be899ebf69eba754555be
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 15:39:29 +0200

    V3 deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel in deploy v2!! 
+                    Laravel deploy v3!! 
                 </div>
 
                 <div class="links">
