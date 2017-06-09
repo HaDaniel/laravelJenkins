b7d7b3174e943b87f9b3ce9a2548819dd06548a6
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 14:19:35 +0200

    Add logs route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
 Route::get('/home', 'HomeController@index')->name('home');
