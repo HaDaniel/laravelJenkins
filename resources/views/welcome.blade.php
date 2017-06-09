f45a48a54fd39c39735fe36cb5709bee6e2dd316
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 11:45:56 +0200

    Add rocketter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel in deploy !! 
                 </div>
 
                 <div class="links">
