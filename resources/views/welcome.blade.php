4f845ad675fe7786e0fced2b222a61821ae479d5
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 16:27:31 +0200

    deploy prod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel deploy 54!!
+                    Laravel deploy !!
                 </div>
 
                 <div class="links">
