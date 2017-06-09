f9f84170fdc83ba6ba5fbbf4e6147cf25dcb7a6c
HaDaniel <ha_daniel@hotmail.fr>
2017-06-09 16:00:25 +0200

    Deploy rocketeer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel deploy v3!! 
+                    Laravel deploy!! 
                 </div>
 
                 <div class="links">
